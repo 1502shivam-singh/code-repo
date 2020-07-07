a40a5807cb88fa5ee27e8b83ec4491a94534e4f1
Shivam Singh <57267960+1502shivam-singh@users.noreply.github.com>
2020-07-07 23:47:34 +0530

    Added updated source code

--- PyScripter/Source.cpp
+++ PyScripter/Source.cpp
@@ -1,1 +1,36 @@
+#include<iostream>
+#include<conio.h>
+#include<fstream>
+#include<string>
 
+int main() {
+	std::string file_data;
+	std::ofstream fout;
+	std::ifstream fin;
+	std::string string;
+
+	std::cout << "Write your code below, to end code session write -1 and press enter :" << std::endl << std::endl;
+	fout.open("SearchFile.py");
+
+	while (fout)
+	{
+		std::getline(std::cin, file_data);
+		if (file_data == "-1") { break; }
+		fout << file_data<<std::endl;	
+	}
+	fout.close();
+	file_data.erase();
+	std::cout <<std::endl<< "Your entered code"<<std::endl<<std::endl;
+	fin.open("SearchFile.py");
+	
+	while (fin) {
+		std::getline(fin, file_data);
+		std::cout << file_data<<std::endl;
+	}
+
+	fin.close();
+	std::cout <<std::endl<< "Output" << std::endl;
+	system("SearchFile.py");
+
+	_getch();
+}
